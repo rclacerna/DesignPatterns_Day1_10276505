3d08c4b32056d3f80b7130bc818247d83b39e6b3
Ryan Lacerna <rclacerna@gmail.com>
2018-12-03 22:12:53 +0000

    update main.rb

--- main.rb
+++ main.rb
@@ -15,85 +15,34 @@ oracle = Oracle.new
 # evaluate random strategy
 total_num_attempts = 0
 total_num_failures = 0
-random = Play_randomly.new(oracle, max_num_attempts: NUM_OF_RUNS*2, lower: 1, upper: NUM_OF_RUNS)
+
+choice = Kernel.rand(1..4)
+
+case choice
+when 1
+  strategy = Play_randomly.new(oracle, max_num_attempts: NUM_OF_RUNS*2, lower: 1, upper: NUM_OF_RUNS)
+  title = "randomly"
+when 2
+  strategy = Play_linear.new(oracle, max_num_attempts: NUM_OF_RUNS*2, lower: 1)
+    title = "linear"
+when 3
+  strategy = Play_smart_random.new(oracle, max_num_attempts: NUM_OF_RUNS*2, lower: 1, upper: NUM_OF_RUNS)
+  title = "smart"
+  when 4
+  strategy = Play_binary_search.new(oracle, max_num_attempts: NUM_OF_RUNS*2, lower: 1, upper: NUM_OF_RUNS)
+  title = "binary search"
+end
 
 1.upto(NUM_OF_RUNS) do |i|
   oracle.secret_number = i
   participant = Participant.new
-  participant.strategy = random
+  participant.strategy = strategy
   if participant.start_play == :success
-    # puts "play randomly found #{i} in #{random.num_attempts} attempts"
-    total_num_attempts += random.num_attempts
+    # puts "play #{title} found #{i} in #{random.num_attempts} attempts"
+    total_num_attempts += strategy.num_attempts
   else
-    # puts "play randomly failed to find #{i} after #{random.num_attempts} attempts"
+    # puts "play #{title} failed to find #{i} after #{random.num_attempts} attempts"
     total_num_failures += 1
   end
 end
-puts "play randomly took on average #{total_num_attempts/(NUM_OF_RUNS-total_num_failures)} attempts to succeed"
-
-
-# evaluate linear strategy
-total_num_attempts = 0
-total_num_failures = 0
-
-linear = Play_linear.new(oracle, max_num_attempts: NUM_OF_RUNS*2, lower: 1)
-
-1.upto(NUM_OF_RUNS) do |i|
-  oracle.secret_number = i
-  participant = Participant.new
-  participant.strategy = linear
-
-  if participant.start_play == :success
-    # puts "play randomly found #{i} in #{random.num_attempts} attempts"
-    total_num_attempts += linear.num_attempts
-  else
-    # puts "play randomly failed to find #{i} after #{random.num_attempts} attempts"
-    total_num_failures += 1
-  end
-
-end
-puts "play_linear took on average #{total_num_attempts/(NUM_OF_RUNS-total_num_failures)} attempts to succeed"
-
-
-# evaluate 'smart random' strategy
-total_num_attempts = 0
-total_num_failures = 0
-smart = Play_smart_random.new(oracle, max_num_attempts: NUM_OF_RUNS*2, lower: 1, upper: NUM_OF_RUNS)
-
-1.upto(NUM_OF_RUNS) do |i|
-  oracle.secret_number = i
-  participant = Participant.new
-  participant.strategy = smart
-
-  if participant.start_play == :success
-    # puts "play randomly found #{i} in #{random.num_attempts} attempts"
-    total_num_attempts += smart.num_attempts
-  else
-    # puts "play randomly failed to find #{i} after #{random.num_attempts} attempts"
-    total_num_failures += 1
-  end
-
-end
-
-puts "play_smart_random took on average #{total_num_attempts/(NUM_OF_RUNS-total_num_failures)} attempts to succeed"
-
-# evaluate binary search strategy
-total_num_attempts = 0
-total_num_failures = 0
-binary = Play_binary_search.new(oracle, max_num_attempts: NUM_OF_RUNS*2, lower: 1, upper: NUM_OF_RUNS)
-
-1.upto(NUM_OF_RUNS) do |i|
-  oracle.secret_number = i
-  participant = Participant.new
-  participant.strategy = binary
-
-  if participant.start_play == :success
-    # puts "play randomly found #{i} in #{random.num_attempts} attempts"
-    total_num_attempts += binary.num_attempts
-  else
-    # puts "play randomly failed to find #{i} after #{random.num_attempts} attempts"
-    total_num_failures += 1
-  end
-
-end
-puts "play_binary_search took on average #{total_num_attempts/(NUM_OF_RUNS-total_num_failures)} attempts to succeed"
+puts "play #{title} took on average #{total_num_attempts/(NUM_OF_RUNS-total_num_failures)} attempts to succeed"
